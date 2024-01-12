3b42b54978f31a23edbe3ecd71e667a253033c05
Armando Magana <magana.armando52@gmail.com>
2024-01-12 15:35:30 -0600

    added components

--- ID-Card/src/app/app-routing.module.ts
+++ ID-Card/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IDInformationComponent } from './Components/id-information/id-information.component';
+import { UserComponent } from './Components/user/user.component';
+import { ManagerComponent } from './Components/manager/manager.component';
+import { DepartmentComponent } from './Components/department/department.component';
+import { BuildingComponent } from './Components/building/building.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/id-service', pathMatch: 'full'},
-  {path: 'id-service', component: IDInformationComponent}
+  {path: 'id-service', component: IDInformationComponent},
+  {path: 'building', component: BuildingComponent},
+  {path: 'department', component: DepartmentComponent},
+  {path: 'manager', component: ManagerComponent},
+  {path: 'user', component: UserComponent}
 ];
 
 @NgModule({
--- ID-Card/src/app/app.module.ts
+++ ID-Card/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IDInformationComponent } from './Components/id-information/id-information.component';
+import { BuildingComponent } from './Components/building/building.component';
+import { DepartmentComponent } from './Components/department/department.component';
+import { ManagerComponent } from './Components/manager/manager.component';
+import { UserComponent } from './Components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IDInformationComponent
+    IDInformationComponent,
+    BuildingComponent,
+    DepartmentComponent,
+    ManagerComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
